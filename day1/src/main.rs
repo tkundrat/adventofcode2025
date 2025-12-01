411b9b3b3a108b75a828a793225da2174f0ba243
Tristan Kundrat <tristan.kundrat@gmail.com>
2025-12-01 11:26:21 +0100

    finished Day 1 Part 1

---
+++ day1/src/main.rs
@@ -0,0 +1,92 @@
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+    str::FromStr,
+};
+
+struct Dial(u32);
+
+impl Dial {
+    pub fn new() -> Self {
+        Dial(50)
+    }
+
+    fn turn_right(&mut self, amount: u32) {
+        self.0 = (self.0 + amount) % 100;
+    }
+
+    fn turn_left(&mut self, amount: u32) {
+        let mut result = self.0 as i32 - amount as i32;
+        while result < 0 {
+            result += 100
+        }
+        self.0 = result as u32;
+    }
+
+    pub fn turn(&mut self, instruction: DialInstruction) {
+        match instruction.direction {
+            TurnDirection::Left => self.turn_left(instruction.amount),
+            TurnDirection::Right => self.turn_right(instruction.amount),
+        };
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.0 == 0
+    }
+}
+
+enum TurnDirection {
+    Left,
+    Right,
+}
+
+struct DialInstruction {
+    direction: TurnDirection,
+    amount: u32,
+}
+
+impl DialInstruction {
+    pub fn from_line(line: String) -> Result<Self, Box<dyn Error>> {
+        let direction = match &line[..1] {
+            "L" => TurnDirection::Left,
+            "R" => TurnDirection::Right,
+            dir => return Err(format!("Invalid direction `{dir}`!").into()),
+        };
+        let amount = u32::from_str(&line[1..])?;
+
+        Ok(DialInstruction { direction, amount })
+    }
+}
+
+fn file() -> Result<File, Box<dyn Error>> {
+    let mut args = env::args();
+    args.next(); // throw away program name
+
+    let filename = args.next().ok_or("File name not provided!")?;
+    let file = File::open(filename)?;
+
+    Ok(file)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = file()?;
+    let buf_reader = BufReader::new(file).lines();
+
+    let mut dial = Dial::new();
+
+    let mut password: u32 = 0;
+
+    for line in buf_reader {
+        let instruction = DialInstruction::from_line(line?)?;
+        dial.turn(instruction);
+        if dial.is_zero() {
+            password += 1;
+        }
+    }
+
+    println!("Password is: {password}");
+
+    Ok(())
+}
