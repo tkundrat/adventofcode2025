6cf88463a81403fdc221a7dea6947f2ca1118c52
Tristan Kundrat <tristan.kundrat@gmail.com>
2025-12-02 20:34:18 +0100

    finished Day 2 Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,9 +1,33 @@
 use std::{env, error::Error, fs::read_to_string};
 
-fn is_invalid(number: u64) -> bool {
+fn is_invalid_one(number: u64) -> bool {
     let log10_num = (number as f64).log10().ceil() as u32;
 
     (number / 10u64.pow(log10_num / 2)) == (number % 10u64.pow(log10_num / 2))
+}
+
+fn is_invalid_two(number: u64) -> bool {
+    let log10_num = (number as f64).log10().ceil() as u32;
+
+    (1..=(log10_num / 2))
+        .filter(|s| {
+            if log10_num.is_multiple_of(*s) {
+                let mut number = number;
+                let pattern = number % 10u64.pow(*s);
+                while number > 0 {
+                    if pattern != number % 10u64.pow(*s) {
+                        return false;
+                    }
+                    number /= 10u64.pow(*s);
+                }
+                true
+            } else {
+                false
+            }
+        })
+        .peekable()
+        .peek()
+        .is_some()
 }
 
 fn file_string() -> Result<String, Box<dyn Error>> {
@@ -17,7 +41,7 @@ fn file_string() -> Result<String, Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let invalid_id_sum = file_string()?
+    let invalid_id_sum_one = file_string()?
         .trim()
         .split(',')
         .map(|range| {
@@ -25,11 +49,25 @@ fn main() -> Result<(), Box<dyn Error>> {
             let start: u64 = range.next().ok_or("Start value invalid")?.parse()?;
             let end: u64 = range.next().ok_or("End value invalid")?.parse()?;
 
-            Ok::<u64, Box<dyn Error>>((start..=end).filter(|n| is_invalid(*n)).sum())
+            Ok::<u64, Box<dyn Error>>((start..=end).filter(|n| is_invalid_one(*n)).sum())
         })
         .sum::<Result<u64, _>>()?;
 
-    println!("Invalid ID sum: {invalid_id_sum}");
+    println!("Invalid ID sum (Part one): {invalid_id_sum_one}");
+
+    let invalid_id_sum_two = file_string()?
+        .trim()
+        .split(',')
+        .map(|range| {
+            let mut range = range.split('-');
+            let start: u64 = range.next().ok_or("Start value invalid")?.parse()?;
+            let end: u64 = range.next().ok_or("End value invalid")?.parse()?;
+
+            Ok::<u64, Box<dyn Error>>((start..=end).filter(|n| is_invalid_two(*n)).sum())
+        })
+        .sum::<Result<u64, _>>()?;
+
+    println!("Invalid ID sum (Part two): {invalid_id_sum_two}");
 
     Ok(())
 }
